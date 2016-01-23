9c32d8da5e2b7e15f9077808a10888cdf9898153
Peter Scheibel <scheibel1@llnl.gov>
2016-01-22 16:37:14 -0800

    generated rpm was missing base directories

--- lib/spack/spack/cmd/rpm-install.py
+++ lib/spack/spack/cmd/rpm-install.py
@@ -64,11 +64,11 @@ SOURCE0 : %{{name}}-%{{version}}.tar.gz
 
 %install
 rm -rf %{{buildroot}}
-mkdir -p  %{{buildroot}}
+mkdir -p  %{{buildroot}}/{3}
 # ./bin/spack install {0}
 # cp -a `find opt/ -name "*{0}-*"`/* %{{buildroot}}
-./bin/spack install --install-dir={3} {0}
-cp -a {3}/* %{{buildroot}}
+./bin/spack install --install-dir=/{3} {0}
+cp -a /{3}/* %{{buildroot}}/{3}
 
 %clean
 rm -rf %{{buildroot}}
@@ -110,7 +110,7 @@ def rpm_install(parser, args):
     
     #import pdb; pdb.set_trace()
     
-    rpmSpecs = generate_specs(topSpec, set(), '/usr/local/testbar/')
+    rpmSpecs = generate_specs(topSpec, set(), 'usr/local/testbar/')
     for rpmName, spec in rpmSpecs:
         with open(os.path.join(args.outputDir, "%s.spec" % rpmName), 'wb') as F:
             F.write(spec)
