c17c387ab2ebbd6c2a62f1192f0e27976187e728
Peter Scheibel <scheibel1@llnl.gov>
2026-06-08 16:42:10 -0700

    Proper handling of SetEnv
    Signed-off-by: Peter Scheibel <scheibel1@llnl.gov>

--- lib/spack/spack/util/environment.py
+++ lib/spack/spack/util/environment.py
@@ -285,6 +285,9 @@ class NameValueModifier:
         """Apply the modification to the mapping passed as input"""
         raise NotImplementedError("must be implemented by derived classes")
 
+    def __hash__(self):
+        return hash((self.name, self.value, self.separator))
+
 
 class NamePathModifier(NameValueModifier):
     """Base class for modifiers that modify the value of an environment variable
@@ -301,9 +304,6 @@ class NamePathModifier(NameValueModifier):
         trace: Optional[Trace] = None,
     ):
         super().__init__(name, str(value), separator=separator, trace=trace)
-
-    def __hash__(self):
-        return hash((self.name, self.value, self.separator))
 
 
 class SetEnv(NameValueModifier):
